89fed53178294524fe1cfb5a6f52a0fe8743fa0d
GuilhermeRoehers <guilherme.roehers09@gmail.com>
2024-10-24 15:26:21 -0300

    Add files via upload

---
+++ trabalho.js
@@ -0,0 +1,138 @@
+// 1. Crie uma variável chamada `nome` e atribua seu nome a ela. Exiba o nome no console.
+let nome = "guilherme"
+console.log(nome)
+
+
+// 2. Crie uma constante chamada `PI` e atribua o valor 3.14. Exiba o valor no console.
+const numero = 3.14
+console.log(numero)
+
+
+
+// 3. Crie uma variável `idade`, atribua um valor e depois altere para um novo valor. Exiba ambos no console.
+let idade = 15 
+console.log(15-1)
+
+
+// 4. Crie duas variáveis de string, `primeiroNome` e `sobrenome`, e exiba o nome completo concatenado.
+let primeiroNome = "Guilherme"
+let segundoNome = "oliveira"
+console.log(`meu nome: ${primeiroNome} ${segundoNome}`)
+
+/*
++ :     "Guilherme" + " Oliveira"
+,  :     console.log(primeiroNome, segundoNome)
+template string: `meu nome: ${primeiroNome} ${segundoNome}`
+*/
+
+
+// 5. Dada a string `mensagem = "Olá, mundo!"`, exiba a mensagem em letras maiúsculas.
+let frase = "Olá Mundo"
+let fraseMaiuscula = frase.toLocaleUpperCase()
+console.log(fraseMaiuscula)
+
+
+
+// 6. Crie um array com os nomes de três frutas e exiba o segundo elemento do array.
+const listaDEfrutas = ["banana", "abacaxi" , "uva"]
+const segundoIten = listaDEfrutas[1]
+console.log(segundoIten)
+
+// 7. Crie um array com três números e adicione um novo número no final. Exiba o array atualizado.
+let numeros = ["5", "6" , "7"]
+numeros.push(8)
+console.log(numeros)
+
+
+
+// 8. Crie duas variáveis, `a` e `b`, atribua valores numéricos a elas e exiba a soma.
+let primeiroNumero = 58
+let segundoNumero2 = 43
+console.log(primeiroNumero + segundoNumero2)
+
+// 9. Calcule a média de três números armazenados em variáveis e exiba o resultado.
+let primeiroNumero2 = 54
+let segundoNumero3 = 32
+let terceiroNumero = 26
+console.log(primeiroNumero2 + segundoNumero3 + terceiroNumero)
+
+// 10. Crie uma variável `contador` inicializada em 0, incremente-a três vezes e exiba seu valor final.
+let contador = 0
+contador = contador +3
+console.log(contador)
+
+
+
+// 11. Crie duas variáveis booleanas, `chuvendo` e `frio`, e use um operador lógico para verificar se deve levar um guarda-chuva.
+let chuvendo = true // hoje está chovendo
+let frio = false // hoje faz muito calor
+
+let levarguardachuva = chuvendo || frio // levar guarda chuva se tiver chovendo
+console.log('devo levar uma guarda chuva ?' , levarguardachuva)
+
+
+
+
+  
+    
+
+
+// 12. Crie uma variável `idade` e use um comparador para verificar se a pessoa pode dirigir (idade >= 18).
+let idade2 = 23
+let podedirigir = idade2 >= 18
+console.log("pode dirigir ?" , podedirigir)
+
+
+
+
+// 13. Crie duas variáveis `x` e `y`, atribua valores diferentes e verifique se são iguais.
+let x = 20
+let y = 10
+let saoIguais = (x==y)
+console.log(saoIguais)
+
+
+
+
+
+// 14. Crie uma variável `idade` e verifique se a pessoa é maior de idade (idade >= 18) e exiba uma mensagem correspondente.
+let idade3 = 15
+let maiorIdade = (15==18)
+console.log(maiorIdade)
+
+
+
+
+
+// 15. Crie duas strings, `senha1` e `senha2`, e verifique se elas são iguais. 
+let senha1
+let senha2
+console.log(senha1==senha2)
+
+
+
+
+// 16. Crie uma string e exiba o número de caracteres que ela contém.
+ nome= "Olá, mundo!"
+ numero_de_caracteristicas = (nome)
+ console.log(numero_de_caracteristicas)
+
+
+
+
+
+// 18. Crie uma variável numérica e verifique se é par ou ímpar usando um operador aritmético.
+let numerica = 3
+let impar
+let par
+console.log(numerica==par)
+
+
+
+
+
+
+// 20. Crie duas variáveis, `nota1` e `nota2`, calcule a média e verifique se a média é maior ou igual a 7
+let nota1 = 4
+let nota2 = 3
+console.log(nota1 + nota2)
